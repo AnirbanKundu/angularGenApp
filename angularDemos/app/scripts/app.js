79ae760b8efc71d41409c1b1c6809fd0392a037a
a.kundu@ge.com <a.kundu@ge.com>
2014-05-03 01:45:21 -0700

    i18n code check-in

--- angularDemos/app/scripts/app.js
+++ angularDemos/app/scripts/app.js
@@ -1,11 +1,12 @@
 'use strict';
 
-angular
+var angularDemosApp =  angular
   .module('angularDemosApp', [
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngRoute'
+    'ngRoute',
+    'pascalprecht.translate'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -17,3 +18,14 @@ angular
         redirectTo: '/'
       });
   });
+
+  angularDemosApp.config(['$translateProvider','$translatePartialLoaderProvider', function ($translateProvider,$translatePartialLoaderProvider) {
+  $translateProvider.useLoader('$translatePartialLoader', {
+        urlTemplate: 'i18n/{lang}/{part}.json'
+      });
+    var userLaunguage = navigator.language || navigator.browserLanguage; //This is for IE.
+    $translateProvider.preferredLanguage(userLaunguage.toLowerCase()); //de_DE -->keep this so that it easy to test for other languages de-DE --? GERMAN
+}]);
+
+
+
--- angularDemos/app/scripts/controllers/main.js
+++ angularDemos/app/scripts/controllers/main.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('angularDemosApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($rootScope,$scope,$translate,$translatePartialLoader) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $translatePartialLoader.addPart('mainpage');
+	$translate.refresh();
+
+  	$rootScope.$on('$translateChangeSuccess', function () {
+	    $translate('VARIABLE_REPLACEMENT2').then(function (VARIABLE_REPLACEMENT2) {
+	      $scope.VARIABLE_REPLACEMENT2 = VARIABLE_REPLACEMENT2;
+	    });
+	});
+
   });
